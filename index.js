63527fb3c99710410f0163701aae338e79fc2902
mjahanmktx <123376809+mjahanmktx@users.noreply.github.com>
2023-01-25 10:42:43 -0500

    1 file modified

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
-async function getUsers() {
-  const response = await fetch('https://jsonplaceholder.typicode.com/users/');
-  const users = await response.json();
-  console.log(users);
+export default function App() {
+  const [joke, setJoke] = React.useState('Click the button to get a joke...');
+  return (
+    <div>
+      <h3>Chuck Norris Jokes from chucknorris.io</h3>
+      <span>{joke}</span>
+      <p>
+        <form>
+          <button
+            onClick={(e) => {
+              e.preventDefault();
+              fetch('https://api.chucknorris.io/jokes/random')
+                .then((response) => response.json())
+                .then((json) => setJoke(json.value))
+                .catch(console.error);
+            }}
+          >
+            Get Joke
+          </button>
+        </form>
+      </p>
+    </div>
+  );
 }
-
-getUsers().catch(console.error);
